78cc1382dd2083b07662d128410df1e18f3962e5
Thang <54808964+thang040602@users.noreply.github.com>
2019-09-24 22:42:19 +0700

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
 document.getElementById("le1").onclick = function()
 {
-    document.getElementById("frmshow").src = "lesson1.html";
+    document.getElementById("frmshow").src = "lesson1/lesson1.html";
 }
 
 document.getElementById("le2").onclick = function()
 {
-    document.getElementById("frmshow").src = "lesson2.html";
+    document.getElementById("frmshow").src = "lesson2/lesson2.html";
 }
