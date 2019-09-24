e717f17377407efe7167dcee550c90bea1b5f644
Thang <54808964+thang040602@users.noreply.github.com>
2019-09-24 22:40:30 +0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+document.getElementById("le1").onclick = function()
+{
+    document.getElementById("frmshow").src = "lesson1.html";
+}
+
+document.getElementById("le2").onclick = function()
+{
+    document.getElementById("frmshow").src = "lesson2.html";
+}
