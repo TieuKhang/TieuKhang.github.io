80b857f32b5a3b88f403ef7b60a58a9678d90c79
Thang <54808964+thang040602@users.noreply.github.com>
2019-09-26 23:37:09 +0700

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,21 @@
+var kCount = 2;
+
+function reset_col(elim) {
+    for (var i = 1; i < kCount + 1; ++i) {
+        var name = "le" + i.toString();
+        document.getElementById(name).style.backgroundColor = "#565d69"
+    }
+    document.getElementById(elim).style.backgroundColor = "#43454a"
+}
+
 document.getElementById("le1").onclick = function()
 {
-    document.getElementById("frmshow").src = "lesson1/lesson1.html";
+    document.getElementById("frmshow").src = "lesson1.html";
+    reset_col("le1");
 }
 
 document.getElementById("le2").onclick = function()
 {
-    document.getElementById("frmshow").src = "lesson2/lesson2.html";
+    document.getElementById("frmshow").src = "lesson2.html";
+    reset_col("le2");
 }
